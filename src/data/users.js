dc8ba648a14ed08c4d8fbefcf97f9eb4a1458dd0
Bartosz Łyśniewski <bartosz@iMac-Bartosz.local>
2022-05-03 02:38:55 +0200

    user-list-component

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        name: 'Adam Romański',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Krzystof Batko',
+        attendance: '41',
+        average: '2.5',
+    },
+    {
+        name: 'Patrycja Gonciarz',
+        attendance: '59%',
+        average: '3.4',
+    },
+    {
+        name: 'Olga Hahn',
+        attendance: '54%',
+        average: '4.5',
+    },
+    {
+        name: 'Paweł Andrzejewski',
+        attendance: '60%',
+        average: '4.6',
+    },
+    {
+        name: 'Szymon Knuth',
+        attendance: '69%',
+        average: '4.1',
+    },
+    {
+        name: 'Krystian Foczpaniak',
+        attendance: '67%',
+        average: '3.7',
+    },
+]
